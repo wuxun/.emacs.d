72bfa6ce54a7b927e8b95971839452edae2efe1e
wuxun <wuxun@cvte.cn>
2013-09-27 10:21:58 +0800

    [Feature]add emacs initial commit

---
+++ emacs.el
@@ -0,0 +1,34 @@
+(add-to-list 'load-path "~/.emacs.d/plugins")
+(load "whitespace.el")
+
+(global-linum-mode t)
+
+(require 'xcscope)
+
+;;(require 'whitespace)
+;;(global-whitespace-mode)
+;;(setq whitespace-style
+;;            '(face trailing tabs lines lines-tail empty
+;;			   space-after-tab space-before-tab))
+;;(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(require 'iswitchb)
+(iswitchb-default-keybindings)
+
+(global-set-key (kbd "RET") 'newline-and-indent)
+
+(add-hook 'c-mode-common-hook (lambda()
+				(c-set-style "linux")))
+
+(add-hook 'java-mode-hook (lambda()
+			    (setq c-basic-offset 4)
+			    (setq-default indent-tabs-mode nil)))
+
+(setq backup-directory-alist (quote (("." . "~/.backups"))))
+
+(global-set-key (kbd "<f5>") 'delete-trailing-whitespace)
+;;(add-hook 'before-save-hook (lambda ()
+;;			      (whitespace-cleanup)))
+
+;;(add-hook 'before-save-hook (lambda ()
+;;			      (delete-trailing-whitespace)))
