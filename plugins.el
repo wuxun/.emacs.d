32e90fa5791f4ed0d66a78abb086008fbac47cf5
wuxun <wuxunyidao@gmail.com>
2014-12-26 21:44:01 +0800

    add yasnippet

--- plugins.el
+++ plugins.el
@@ -3,3 +3,9 @@
 (require 'auto-complete-config)
 (ac-config-default)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/plugins/auto-complete-1.3.1/dict")
+
+;;for yasnippet
+(add-to-list 'load-path
+              "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet)
+(yas-global-mode 1)
