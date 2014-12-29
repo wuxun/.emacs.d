47fbd951b692a352edfecfec4cdc6e21eca6609b
wuxun <wuxunyidao@gmail.com>
2014-12-29 15:32:10 +0800

    uses display-graphic-p instead of window-system

--- interface.el
+++ interface.el
@@ -7,21 +7,21 @@
 
 (defun set-frame-size-according-to-resolution ()
   (interactive)
-  (if window-system
-  (progn
-    ;; use 120 char wide window for largeish displays
-    ;; and smaller 80 column windows for smaller displays
-    ;; pick whatever numbers make sense for you
-    ;;(if (> (x-display-pixel-width) 1280)
-    ;;       (add-to-list 'default-frame-alist (cons 'width 120))
-    ;;       (add-to-list 'default-frame-alist (cons 'width 80)))
-    (add-to-list 'default-frame-alist (cons 'width 80))
-    ;; for the height, subtract a couple hundred pixels
-    ;; from the screen height (for panels, menubars and
-    ;; whatnot), then divide by the height of a char to
-    ;; get the height we want
-    (add-to-list 'default-frame-alist 
-         (cons 'height (/ (- (x-display-pixel-height) 120)
-                             (frame-char-height)))))))
+  (if (display-graphic-p)
+      (progn
+	;; use 120 char wide window for largeish displays
+	;; and smaller 80 column windows for smaller displays
+	;; pick whatever numbers make sense for you
+	;;(if (> (x-display-pixel-width) 1280)
+	;;       (add-to-list 'default-frame-alist (cons 'width 120))
+	;;       (add-to-list 'default-frame-alist (cons 'width 80)))
+	(add-to-list 'default-frame-alist (cons 'width 80))
+	;; for the height, subtract a couple hundred pixels
+	;; from the screen height (for panels, menubars and
+	;; whatnot), then divide by the height of a char to
+	;; get the height we want
+	(add-to-list 'default-frame-alist
+		     (cons 'height (/ (- (x-display-pixel-height) 120)
+				      (frame-char-height)))))))
 
 (set-frame-size-according-to-resolution)
