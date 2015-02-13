44757e97cb3c93b91e245035a1179e67bda2bbcb
wuxun <wuxunyidao@gmail.com>
2015-02-14 00:15:03 +0800

    add helm plugin and adjust xml code style

--- code_style.el
+++ code_style.el
@@ -15,3 +15,9 @@
 (add-hook 'c++-mode-hook (lambda()
                            (setq c-basic-offset 4)
                            (setq-default indent-tabs-mode nil)))
+
+;; nxml
+(add-hook 'nxml-mode-hook (lambda()
+                            (setq indent-tabs-mode nil)
+                            (setq nxml-child-indent 4)
+                            (setq nxml-attribute-indent 4)))
--- plugins.el
+++ plugins.el
@@ -13,3 +13,12 @@
 ;;for emacs-color-theme-solarized
 (add-to-list 'custom-theme-load-path "~/.emacs.d/plugins/emacs-color-theme-solarized")
 (load-theme 'solarized-dark t)
+
+;;for helm
+(add-to-list 'load-path "~/.emacs.d/plugins/helm")
+(add-to-list 'load-path "~/.emacs.d/plugins/emacs-async")
+(require 'helm-config)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
